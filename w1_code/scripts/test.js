92b49661287b2b5287424ba3699e9ad00b2507a8
xilibi2003@163.com <xilibi2003@163.com>
2022-02-04 23:24:03 +0800

    add week1 code1

---
+++ w1_code/scripts/test.js
@@ -0,0 +1,15 @@
+var Counter = artifacts.require("Counter");
+
+module.exports = async function(callback) {
+  var counter = await Counter.deployed()
+
+  await counter.count();
+  let value = await counter.counter();
+  
+  console.log("current conter value:" + value); 
+}
+
+
+
+  // accounts = await web3.eth.getAccounts()
+  // process.exit(0);
