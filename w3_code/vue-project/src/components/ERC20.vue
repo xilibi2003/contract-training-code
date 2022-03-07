a0c2ae9e9c3b5034526ece9619392021aab4220b
xilibi2003@163.com <xilibi2003@163.com>
2022-03-07 21:19:45 +0800

    init vue frame

---
+++ w3_code/vue-project/src/components/ERC20.vue
@@ -0,0 +1,38 @@
+<script>
+
+
+
+
+</script>
+
+<template>
+  <div >
+    <h3>
+      You’ve successfully created a project
+    </h3>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
